87021de3b2022a799356e119148aabc8749ca16d
JASHWANTHDB <jashwanthb43@gmail.com>
2025-10-29 12:03:56 +0530

    Updated game logic and UI

--- src/com/namics/AddServlet.java
+++ src/com/namics/AddServlet.java
@@ -1,35 +1,35 @@
-package com.namics;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.http.HttpRequest;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-//@WebServlet("/add")
-public class AddServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
-		int i =Integer.parseInt(req.getParameter("num1"));
-		int j =Integer.parseInt(req.getParameter("num2"));
-		
-		int k = i + j;
-		
-		PrintWriter out= res.getWriter();
-		out.println("<html><body bgcolor='cyan'>");
-		out.println("k"+k);
-		out.print("</body></html>");
-	   res.sendRedirect("sq?k="+k);
-		
-		req.setAttribute("k",k);
-	
-	RequestDispatcher rd = req.getRequestDispatcher("sq");
-	rd.forward(req, res);
-		
-	}
-}
+//package com.namics;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.net.http.HttpRequest;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+////@WebServlet("/add")
+//public class AddServlet extends HttpServlet{
+//	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
+//		int i =Integer.parseInt(req.getParameter("num1"));
+//		int j =Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k = i + j;
+//		
+//		PrintWriter out= res.getWriter();
+//		out.println("<html><body bgcolor='cyan'>");
+//		out.println("k"+k);
+//		out.print("</body></html>");
+//	   res.sendRedirect("sq?k="+k);
+//		
+//		req.setAttribute("k",k);
+//	
+//	RequestDispatcher rd = req.getRequestDispatcher("sq");
+//	rd.forward(req, res);
+//		
+//	}
+//}
--- src/com/namics/hf.java
+++ src/com/namics/hf.java
@@ -1,34 +1,34 @@
-package com.namics;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class hf
- */
-@WebServlet("/hf")
-public class hf extends HttpServlet {
-
-    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        String t = "jfvbasjfsvsgjf";
-        
-        out.println("<html>");
-        out.println("<head><title>" + t + "</title></head>");
-        out.println("<body>");
-        out.println("<h1>" + t + "</h1>");
-        out.println("<ul>");
-        out.println("<li><b>First:</b> " + req.getParameter("first") + "</li>");
-        out.println("<li><b>Last:</b> " + req.getParameter("last") + "</li>");
-        out.println("</ul>");
-        out.println("</body>");
-        out.println("</html>");
-    }
-}
+//package com.namics;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * Servlet implementation class hf
+// */
+//@WebServlet("/hf")
+//public class hf extends HttpServlet {
+//
+//    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//        res.setContentType("text/html");
+//        PrintWriter out = res.getWriter();
+//        String t = "jfvbasjfsvsgjf";
+//        
+//        out.println("<html>");
+//        out.println("<head><title>" + t + "</title></head>");
+//        out.println("<body>");
+//        out.println("<h1>" + t + "</h1>");
+//        out.println("<ul>");
+//        out.println("<li><b>First:</b> " + req.getParameter("first") + "</li>");
+//        out.println("<li><b>Last:</b> " + req.getParameter("last") + "</li>");
+//        out.println("</ul>");
+//        out.println("</body>");
+//        out.println("</html>");
+//    }
+//}
